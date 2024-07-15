e3b63b69edc24db1b82d232d5c9df3c3a11c78f5
gmanne11 <gopim4959@gmail.com>
2024-07-15 18:16:10 +0530

    adding jenkins bucket name to outputs file

--- Terraform-code-jenkins-setup/outputs.tf
+++ Terraform-code-jenkins-setup/outputs.tf
@@ -5,3 +5,7 @@ output "bastion_host_instance_id" {
 output "lb_dns_name" {
     value = module.alb.aws_lb_dns_name
 }
+
+output "jenkins_s3_bucket_name" {
+    value = module.S3.jenkins_bucket_name
+}
