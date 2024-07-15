0623ba466fc63d36b704adf65c4925406c0ef603
gmanne11 <gopim4959@gmail.com>
2024-07-15 18:26:02 +0530

    enable s3 backend for jenkins setup

--- Terraform-code-jenkins-setup/remote_backend_s3.tf
+++ Terraform-code-jenkins-setup/remote_backend_s3.tf
@@ -1,8 +1,8 @@
-/*terraform { # Make sure to create this bucket manually before configuring it as backend.
+terraform { # Make sure to create this bucket manually before configuring it as backend.
   backend "s3" {
     bucket = "my-dev-demo-jenkins-bucket"
     key = "devops-project-1/jenkins/terraform.tfstate"
     region = "us-east-1"
     
   }
-}*/
+}
